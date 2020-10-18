1e7c113ca6bd6a7cb76bc9876bb9c9bcad74b4a1
Tony Zeidan <tony.azp25@gmail.com>
2020-10-18 17:04:26 -0400

    Landmass.java added. It contains functionality for region printing and determining conquerors.

---
+++ src/Landmass.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Landmass represents a region in which smaller territories are contained (ex. Continents)
+ *
+ * @author Tony Zeidan
+ *
+ */
+public class Landmass {
+
+    /**
+     * Name of the region that this instance represents.
+     */
+    private String name;
+
+    /**
+     * List of territories within the region that this instance represents.
+     */
+    private List<Territory> territories;
+
+    /**
+     * Constructor for instances of Landmass. Creates a new (empty) region with
+     * the given name.
+     *
+     * @param name The name of the region.
+     */
+    public Landmass(String name) {
+        this.name=name;
+        territories = new ArrayList<>();
+    }
+
+    /**
+     *  (For future use)
+     *  Determines whether a single Player owns all territories within the region.
+     *  (For bonus unit placement)
+     *
+     * @return The player who owns all territories (or null)
+     */
+    public Player getConqueror() {
+        Player conqueror = territories.get(0).getOwner();
+        for (int i = 1; i < territories.size(); i++) {
+            if (territories.get(i).getOwner()!=territories.get(i-1).getOwner()) return null;
+        }
+        return conqueror;
+    }
+
+    /**
+     * Have a pretty version of the Landmass and its contained territories printed.
+     */
+    public void print() {
+        System.out.println("Landmass of " + name + ": ");
+        for (Territory t : territories) {
+            System.out.println("\t"+t);
+        }
+    }
+}
