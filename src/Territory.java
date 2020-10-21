75700274ecdec2eb715c286050ff00af1ab6a814
Tony Zeidan <tony.azp25@gmail.com>
2020-10-21 18:21:04 -0400

    Previous version restored (before failed merge)

--- src/Territory.java
+++ src/Territory.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Class Territory represents the individual Territories found within the map.
@@ -13,51 +15,108 @@ import java.util.ArrayList;
  */
 public class Territory {
 
-
-    //The name of the territory
+    /**
+     * The name of the territory.
+     */
     private String name;
-     //The player that currently owns units in this territory.
-    private Player currentPlayer;
+    /**
+     * The units that occupy this territory.
+     */
     private int units;
+    /**
+     * The player that currently owns units in this territory.
+     */
+    private Player owner;
+
+    /**
+     * The map implementation of the neighbouring territories.
+     */
+    private Map<String,Territory> neighbours;
+
     /** Constructor for the territory object that contains a name and current player.
      *
      * @param name The name of the territory
      */
     public Territory(String name){
         this.name = name;
-        units =0;
+        units = 0;
+        owner = null;
+        neighbours = new HashMap<>();
+    }
+
+    /**
+     * Searches the neighbouring territories for one with the specified name.
+     *
+     * @param name The name to search for
+     * @return The territory if found otherwise null
+     */
+    public Territory searchNeighbours(String name) {
+        return neighbours.getOrDefault(name, null);
+    }
+
+    /**
+     * Add a territory to the list of territories neighbouring this territory.
+     *
+     * @param territory The new neighbouring territory
+     */
+    public void addNeighbour(Territory territory) {
+        neighbours.put(territory.getName(),territory);
     }
 
     /** Get the name of the territory.
      *
      * @return The name of the territory
      */
-    public String getName() { return name; }
+    public String getName() {
+        return name;
+    }
 
-    /** Add a unit to the territory.
+    /**
+     * Retrieves the amount of units on this region.
      *
+     * @return The number of territory units
      */
-    public void addUnit(int num){ units+=num;}
+    public int getUnits() {
+        return units;
+    }
 
-    /** Remove a unit from the territory.
-     *
+    /**
+     * Sets the number of units in this territory.
      */
-    public void removeUnit(int num){ units-= num;}
+    public void setUnits(int units) {
+        this.units = units;
+    }
 
     /** Get the player that occupies the territory.
      *
      * @return The player that currently occupies the territory.
      */
-    public Player getCurrentPlayer() { return currentPlayer; }
+    public Player getOwner() {
+        return owner;
+    }
 
     /** Set new player as occupant of the territory.
      *
-     * @param currentPlayer The new player that now occupies the territory
+     * @param owner The new player that now occupies the territory
      */
-    public void setCurrentPlayer(Player currentPlayer) { this.currentPlayer = currentPlayer; }
-    public void addNeighbours(Territory nieghbour){}
-    public void print()
-    {
-        System.out.println(name+" owned by " + currentPlayer.getName()+ " with " + units+" troops");
+    public void setOwner(Player owner) {
+        this.owner = owner;
+    }
+
+    /**
+     * Retrieves a string representation of the territory.
+     *
+     * @return A string representation of the territory
+     */
+    @Override
+    public String toString() {
+        return name+" owned by " + owner.getName()+ " with " + units+" troops";
+    }
+
+    /**
+     * print method for printing individual territories from toString().
+     */
+    public void print(){
+        System.out.println(toString());
     }
 }
