95ad29a96460634ee5b27b1a02341a21092d019e
Tony Zeidan <tony.azp25@gmail.com>
2020-10-21 17:00:53 -0400

    Player.java attack() method logic changed (no need for descending order sort) and printing within the method.

--- src/Player.java
+++ src/Player.java
@@ -1,6 +1,5 @@
 import java.awt.*;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Random;
 
 /**
@@ -86,14 +85,6 @@ public class Player {
         this.colour = colour;
     }
 
-    private void reverse(int[] arr) {
-        for (int i = 0; i < arr.length / 2; i++) {
-            int temp = arr[i];
-            arr[i] = arr[arr.length - i - 1];
-            arr[arr.length - i - 1] = temp;
-        }
-    }
-
     /**
      * The player has the ability to attack other territories owned
      * by other players
@@ -102,34 +93,36 @@ public class Player {
      * @param defendRolls The number of dice the defender is using for this defence
      */
     public int[] attack(int attackRolls, int defendRolls) {
+
+        //random acts as die
         Random rand = new Random();
 
-        //generate random rolls for both attacker and defender
-        int[] attackDice = new int[]{rand.nextInt(6) + 1, rand.nextInt(6) + 1, rand.nextInt(6) + 1};
-        int[] defendDice = new int[]{rand.nextInt(6) + 1, rand.nextInt(6) + 1};
+        //two primitive integer arrays to store random rolls
+        int[] attackDice = new int[attackRolls];
+        int[] defendDice = new int[defendRolls];
+
+        //roll dice (random integer) for both parties and display simultaneously
+        System.out.print("Attacking Rolls:   |");
+        for (int i = 0; i < attackRolls; i++) {
+            attackDice[i] = rand.nextInt(6)+1;
+            System.out.print(" " + attackDice[i] + " |");
+        }
+        System.out.print("\nDefending Rolls:  |");
+        for (int i = 0; i< defendRolls; i++) {
+            defendDice[i] = rand.nextInt(6)+1;
+            System.out.print(" " + defendDice[i] + " |");
+        }
 
         //sort both rolls in descending order
         Arrays.sort(attackDice);
-        reverse(attackDice);
         Arrays.sort(defendDice);
-        reverse(defendDice);
-
-        //Print both sets of rolls in descending order
-        for (int i = 0; i < attackRolls; i++) {
-            System.out.print(attackDice[i] + " | ");
-        }
-        for (int j = 0; j < defendRolls; j++) {
-            System.out.print(defendDice[j] + " | ");
-        }
 
         //Set counter variables for lost units in the attack
         int attackLost = 0;
         int defendLost = 0;
 
-        //If attacker is rolling three dice, ignore the third (lowest) dice
-        if (attackRolls==3) attackRolls = 2;
-
         /*
+        Logic:
         If both attacking rolls are greater than both defending rolls, then defender loses two units.
         If the top defender roll is equal/greater than the top attacking roll while the second defending roll
             is less than the second attacking roll, then both players lose one unit.
@@ -138,8 +131,8 @@ public class Player {
         If the attacker rolls one dice, then check if that roll is greater than the top defender roll or not
             and remove unit accordingly.
          */
-        for (int i = 0; i < attackRolls; i++) {
-            for (int j = i; j < defendRolls; j++) {
+        for (int i = attackRolls-1; i >= 0; i--) {
+            for (int j = defendRolls-(attackRolls-i); j >= 0; j--) {
                 if (attackDice[i] > defendDice[j]) {
                     defendLost += 1;
                     break;
@@ -149,8 +142,7 @@ public class Player {
                 }
             }
         }
-        //Print out the result of the attack with the number of units lost for each player
-        System.out.println("Attack Lost: "+attackLost + "\nDefending Lost: " + defendLost);
+
         //Return the result of the attack via units lost
         return new int[]{attackLost,defendLost};
     }
