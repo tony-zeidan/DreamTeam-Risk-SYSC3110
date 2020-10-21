aeb902b983b8aec29e5815eccb10ebc04c6927e5
Ethan Chase <ethanchase@cmail.carleton.ca>
2020-10-21 17:45:31 -0400

    Updated Battle method with basic Attacker input requests

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Class Game implements the main functionality of the RISK game.
@@ -40,13 +41,39 @@ public class Game {
     }
 
     public void Battle(Player attacker,Territory attacking,Territory defending) {
+
+        Scanner aCommand = new Scanner(System.in);
+
         String input = "";
+
+        String attName = attacker.getName();
+
+        int numAttack = 0;
+        int numDefense = 0;
+
         boolean retreat = false;
         while (!retreat) {
-            attacker.attack(3,2); //TODO: change inputs of attack to user inputs
 
-            if (input.equals("retreat")) retreat=true;
+            System.out.println(attName + " is attacking. "+attName+", would you like to attack or retreat?\n");
+
+            input = aCommand.nextLine();
+
+            if(input.equalsIgnoreCase("attack")){
+                System.out.println("How many attacking dice would you like to use: ");
+                if(input.equalsIgnoreCase("3")){
+                    numAttack = 3;
+                }else if(input.equalsIgnoreCase("2")){
+                    numAttack = 2;
+                }else if(input.equalsIgnoreCase("1")){
+                    numAttack = 1;
+                }
+                attacker.attack(numAttack,numDefense);
+
+            }else if(input.equalsIgnoreCase("retreat")){
+                retreat = true;
+            }
         }
-    }
 
+        System.out.println("Battle is over between "+attName+ " and "+defending.getOwner().getName());
+    }
 }
