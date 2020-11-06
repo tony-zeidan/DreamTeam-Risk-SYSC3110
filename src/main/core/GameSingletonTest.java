ea8ba9df17ca95c29e4221e09d1bd4b327405c6c
Ethan Chase <ethanchase@cmail.carleton.ca>
2020-11-06 16:07:41 -0500

    Got GameSingleton model setup for tests in GameSingletonTest.java

--- src/main/core/GameSingletonTest.java
+++ src/main/core/GameSingletonTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -11,7 +12,7 @@ public class GameSingletonTest {
 
     private GameSingleton gsm;
 
-    ArrayList<Player> players;
+    List<Player> players;
 
     @Before
     public void setUp(){
@@ -20,10 +21,19 @@ public class GameSingletonTest {
         players.add(new Player("Anthony", RiskColour.BLUE));
         players.add(new Player("Tony", RiskColour.YELLOW));
         gsm = GameSingleton.getGameInstance(players);
+        gsm.setUpGame();
     }
 
     @Test
-    public void getNumActivePlayer() {
+    public void getNumActivePlayerStatus() {
         assertEquals(3, gsm.getNumActivePlayer());
+    }
+
+    /**
+     * Test getMaxBattleDie()
+     */
+    @Test
+    public void getMaxBattleDieStatus(){
+        assertEquals(2, gsm.getMaxBattleDie(3,true));
     }
 }
