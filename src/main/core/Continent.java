5d2dafe17bbbab6e62a6f93c448b24b913713e5e
kyler <vergekyler@gmail.com>
2020-11-21 15:28:32 -0500

    Continent.java added

---
+++ src/main/core/Continent.java
@@ -0,0 +1,31 @@
+package main.core;
+
+import java.awt.*;
+import java.util.*;
+
+public class Continent {
+
+    private String name;
+
+    private Set<Territory> territories;
+
+    public Continent(String name){
+        this.name = name;
+        territories = new HashSet<>();
+    }
+
+    public String getContinentName(){
+        return name;
+    }
+
+    public void addContinentTerritory(Territory territory){
+        territories.add(territory);
+    }
+
+    public void removeContinentTerritory(Territory territory){
+        territories.remove(territory);
+    }
+
+
+
+}
