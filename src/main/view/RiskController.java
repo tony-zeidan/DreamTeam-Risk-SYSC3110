ef37ee509442cdabd73f4af30f574b99bd70c1fc
Ethan Chase <ethanchase@cmail.carleton.ca>
2020-11-22 12:00:02 -0500

    Removed todo in RiskController (no need to fix bonus troupe implementation in Controller)

--- src/main/view/RiskController.java
+++ src/main/view/RiskController.java
@@ -180,7 +180,6 @@ public class RiskController extends MouseAdapter implements ActionListener {
                 int bonusUnits = riskView.getBonusUnits();
 
                 if (clickedTerritory!=null) {
-                    //TODO: not sure if I can add to the territory directly and bypass GameSingleton
                     clickedTerritory.addUnits(1);
                     bonusUnits -= 1;
                     riskView.setBonusUnits(bonusUnits);
