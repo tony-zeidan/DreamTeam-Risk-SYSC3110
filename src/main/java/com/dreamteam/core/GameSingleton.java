24c4d11403eff39f6319418cdbecfd5729e8b0de
Tony Zeidan <tony.azp25@gmail.com>
2020-12-06 19:18:26 -0500

    Fixing Errors.

--- src/main/java/com/dreamteam/core/GameSingleton.java
+++ src/main/java/com/dreamteam/core/GameSingleton.java
@@ -109,49 +109,59 @@ public class GameSingleton implements Jsonable {
 
         if (mapData!=null) {
             if (mapData.getName().endsWith(".world")) {
-                if (players.get(0).getColour() == null) {
-                    //six random colors for players
-                    List<RiskColour> randomColors = new LinkedList<>();
-                    randomColors.add(RiskColour.RED);
-                    randomColors.add(RiskColour.GRAY);
-                    randomColors.add(RiskColour.BLUE);
-                    randomColors.add(RiskColour.YELLOW);
-                    randomColors.add(RiskColour.BLACK);
-                    randomColors.add(RiskColour.GREEN);
 
-                    Random rand = new Random();
-
-            /*We must get all player names and generate colours.
-            Loop through players and obtain names through user input.
-            Randomly assign colours.
-            */
-                    for (Player p : players) {
-                        //get this players name
-
-                        int randIndex = rand.nextInt(randomColors.size());
-                        //generate and assign random colours
-                        RiskColour colour = randomColors.get(randIndex);
-                        p.setColour(colour);
-                        randomColors.remove(randIndex);
-                    }
-                }
-
-                //shuffle the order of the players
-                shufflePlayers();
             } else if (mapData.getName().endsWith(".save")) {
                 //TODO: read game data here
             }
         }
 
-        //set the initial amount of active players accordingly
-        setNumActivePlayer(players.size());
+
 
         /*Check if colours have already been assigned to players.
         If not, then assign random colours to players.
          */
 
 
-        //world.setUp(players,mapData);
+        //
+
+
+    }
+
+    public void newGame(ZipFile zf) {
+        if (players.get(0).getColour() == null) {
+            //six random colors for players
+            List<RiskColour> randomColors = new LinkedList<>();
+            randomColors.add(RiskColour.RED);
+            randomColors.add(RiskColour.GRAY);
+            randomColors.add(RiskColour.BLUE);
+            randomColors.add(RiskColour.YELLOW);
+            randomColors.add(RiskColour.BLACK);
+            randomColors.add(RiskColour.GREEN);
+
+            Random rand = new Random();
+
+            /*We must get all player names and generate colours.
+            Loop through players and obtain names through user input.
+            Randomly assign colours.
+            */
+            for (Player p : players) {
+                //get this players name
+
+                int randIndex = rand.nextInt(randomColors.size());
+                //generate and assign random colours
+                RiskColour colour = randomColors.get(randIndex);
+                p.setColour(colour);
+                randomColors.remove(randIndex);
+            }
+        }
+
+        world.setUp(players,zf);
+
+        //set the initial amount of active players accordingly
+        setNumActivePlayer(players.size());
+
+        //shuffle the order of the players
+        shufflePlayers();
 
         notifyHandlers(new RiskEvent(this, RiskEventType.GAME_BEGAN,
                 world.getName()));
@@ -164,7 +174,7 @@ public class GameSingleton implements Jsonable {
         nextPhase();    //beginning should be bonus troupe
     }
 
-    public void readGame(InputStream is) {
+    public void importGame() {
 
     }
 
