afee2b16922840b6c1f310145e943d5c1bf89ccc
Ethan Chase <ethanchase@cmail.carleton.ca>
2020-12-09 12:09:53 -0500

    Updated Javadocs for RiskMapPane Added another TODO for RiskGameException

--- src/main/java/com/dreamteam/core/RiskGameException.java
+++ src/main/java/com/dreamteam/core/RiskGameException.java
@@ -4,6 +4,10 @@ package com.dreamteam.core;
  * TODO: Javadoc for this class
  */
 public class RiskGameException extends RuntimeException {
+    /**
+     * TODO: Javadoc for this method
+     * @param message
+     */
     public RiskGameException(String message) {
         super(message);
     }
